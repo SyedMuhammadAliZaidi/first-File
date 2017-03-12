9ce8920569cbca13014a65f8ee0f57834a315e27
Mohammad Ali <Mohammad Ali>
2017-03-12 07:32:48 +0500

    gradesheet

---
+++ marksheet.js
@@ -0,0 +1,21 @@
+var x = prompt("Enter your numbers");
+ if (x>="80" ) {
+ alert(" A");
+ }
+else if (x>="65")
+ {
+ alert("B");
+ 
+ }
+ else if(x>=50)
+ {
+ alert("C");
+ }
+ else if(x>=40)
+ {
+ alert("D");
+ }
+ else(x>=25)
+ {
+ alert("Faii");
+ }
